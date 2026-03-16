dc892544bd8f1860065dfb041bdea433fea4dff0
Frida678 <fbastida1@student.rccd.edu>
2026-03-15 19:21:19 -0700

    i

--- Lab/Introlab/main.cpp
+++ Lab/Introlab/main.cpp
@@ -1,1 +1,24 @@
+/*
+Author: Frida Bastida
+Purpose: Github/IntroLab
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "hello world!" << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
 
